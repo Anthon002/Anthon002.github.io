9a059fc6fc990fe7a74a8d64ca074ff60af1a967
Anthon002 <chineduanulugwo@gmail.com>
2024-09-01 21:56:56 +0100

    portfolio website

---
+++ PorfolioWebsite/public_html/assets/js/index.js
@@ -0,0 +1,54 @@
+/*!
+=========================================================
+* Meyawo Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+// navbar toggle
+$('#nav-toggle').click(function(){
+    $(this).toggleClass('is-active')
+    $('ul.nav').toggleClass('show');
+});
+
+var visitBtn = document.getElementById("VisitMyWork");
+visitBtn.addEventListener("click",()=>{
+    window.scrollTo({
+        top:1600,
+        behavior: 'smooth'
+    })
+})
+
+var contact = document.getElementById("contact")
+contact.addEventListener("click", ()=>{
+    window.scroll({
+        top:2000,
+        behavior:'smooth'
+    })
+})
